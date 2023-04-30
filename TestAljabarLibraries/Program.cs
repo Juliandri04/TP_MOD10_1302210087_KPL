3649ab7a27d19ed6f80e734974ae36524b9ffd5c
Juliandri04 <94310353+Juliandri04@users.noreply.github.com>
2023-04-30 19:46:56 +0700

    Test AljabarLibraries

---
+++ TestAljabarLibraries/Program.cs
@@ -0,0 +1,10 @@
+﻿using AljabarLibraries;
+
+double[] akarpersamaan = { 1, -3, -10 };
+double[] hasil = { 2, -3 };
+
+double[] AkarPersamaanKuadrat = QuadraticForm.AkarPersamaanKuadrat(akarpersamaan);
+double[] HasilKuadrat = QuadraticForm.HasilKuadrat(hasil);
+
+Console.WriteLine("{" + AkarPersamaanKuadrat[0] + ", " + AkarPersamaanKuadrat[1] + "}");
+Console.WriteLine("{" + HasilKuadrat[0] + ", " + HasilKuadrat[1] + ", " + HasilKuadrat[2] + "}");
